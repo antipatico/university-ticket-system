f47438ec039d1487ab8bdbada525c2a759ad5c0b
antipatico <antipatico@nohost.notld>
2021-03-01 12:15:19 +0100

    Registering models in admin

--- uts/uts_common/admin.py
+++ uts/uts_common/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Ticket)
+admin.site.register(Tag)
+admin.site.register(TicketEvent)
+admin.site.register(Notification)
+admin.site.register(Profile)
