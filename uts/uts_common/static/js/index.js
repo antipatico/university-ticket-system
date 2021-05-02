83cac69e267419ebf7f6e5c24d3edf407d836cb5
antipatico <antipatico@github.com>
2021-05-02 17:23:04 +0200

    Linking API

--- uts/uts_common/static/js/index.js
+++ uts/uts_common/static/js/index.js
@@ -1,107 +1,26 @@
 
+function interval(f,delay) {
+    f();
+    return setInterval(f, delay);
+}
+
 const TicketsApp = {
     delimiters: ['$%','%$'],
     data() {
         return {
             openedTicket: null,
-            recentTickets: [
-                {
-                    "id": 6,
-                    "owner": {
-                        "id": 3,
-                        "name": "org1",
-                        "type": "organization"
-                    },
-                    "status": "CLOSED",
-                    "name": "orgticket",
-                    "description": "test",
-                    "events": [
-                        {
-                            "id": 4,
-                            "owner": {
-                                "id": 3,
-                                "name": "org1",
-                                "type": "organization"
-                            },
-                            "status": "NOTE",
-                            "timestamp": "2021-05-02T13:14:32.821286Z",
-                            "info": ""
-                        },
-                        {
-                            "id": 3,
-                            "owner": {
-                                "id": 1,
-                                "name": "admin admin",
-                                "type": "individual"
-                            },
-                            "status": "INFO_NEEDED",
-                            "timestamp": "2021-05-02T13:10:29.673343Z",
-                            "info": ""
-                        },
-                        {
-                            "id": 2,
-                            "owner": {
-                                "id": 2,
-                                "name": "user user",
-                                "type": "individual"
-                            },
-                            "status": "INFO_NEEDED",
-                            "timestamp": "2021-05-02T13:10:21.319360Z",
-                            "info": ""
-                        }
-                    ],
-                    "tags": [
-                        "afdas",
-                        "lmao!"
-                    ],
-                    "ts_open": "2021-05-02T11:27:12.856469Z",
-                    "ts_last_modified": "2021-05-02T12:50:14.058296Z",
-                    "ts_closed": null,
-                    "is_closed": true
-                },
-                {
-                    "id": 2,
-                    "owner": {
-                        "id": 1,
-                        "name": "admin admin",
-                        "type": "individual"
-                    },
-                    "status": "OPEN",
-                    "name": "ticket1",
-                    "description": "desc1",
-                    "events": [],
-                    "tags": [
-                        "afdas"
-                    ],
-                    "ts_open": "2021-05-02T10:53:01.013125Z",
-                    "ts_last_modified": "2021-05-02T12:40:29.856349Z",
-                    "ts_closed": null,
-                    "is_closed": false
-                },
-                {
-                    "id": 3,
-                    "owner": {
-                        "id": 1,
-                        "name": "admin admin",
-                        "type": "individual"
-                    },
-                    "status": "DUPLICATE",
-                    "name": "ticket2",
-                    "description": "desc2",
-                    "events": [],
-                    "tags": [],
-                    "ts_open": "2021-05-02T10:53:11.376866Z",
-                    "ts_last_modified": "2021-05-02T11:16:30.576092Z",
-                    "ts_closed": null,
-                    "is_closed": false
-                }
-            ]
+            recentTickets: null,
         }
     },
     mounted() {
-        console.log("App mounted")
+        this.recentTicketsTimer = interval(this.getRecentTickets, 60*1000);
     },
     methods: {
+        getRecentTickets() {
+            $.getJSON("/api/v1/recentactivities/", (data) => {
+                this.recentTickets = data;
+            });
+        },
         getIconClass(status) {
             switch (status) {
                 case "OPEN":
