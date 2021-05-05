22dd5efe1b320b204a8bbe5570ddf51ce93087e0
antipatico <antipatico@github.com>
2021-05-05 15:07:08 +0200

    Return 404 when ticket does not exist

--- uts/uts_common/views.py
+++ uts/uts_common/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout, authenticate, login
 
 from uts_common.decorators import next_redirect
 from uts_common.forms import *
+from uts_common.models import Ticket
 
 
 def index(request):
@@ -35,4 +37,6 @@ def local_login(request):
 
 @login_required
 def ticket_details(request, pk):
+    if not Ticket.objects.filter(pk=pk).exists():
+        raise Http404
     return render(request, "uts/ticket-details.html", context={"ticket_id": pk})
