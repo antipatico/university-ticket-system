db02fc76cdd3acece0d45d2fdb6cad57faaf29a8
antipatico <antipatico@github.com>
2021-05-05 14:51:57 +0200

    Preparing subscribed tickets

--- uts/uts_common/static/js/index.js
+++ uts/uts_common/static/js/index.js
@@ -3,12 +3,14 @@ const TicketsApp = {
     data() {
         return {
             recentTickets: null,
-            ownedTickets: null
+            ownedTickets: null,
+            subscribedTickets: null,
         }
     },
     mounted() {
         this.recentTicketsTimer = QACommon.interval(this.getRecentTickets, 20 * 1000);
         this.ownedTicketsTimer = QACommon.interval(this.getOwnedTickets, 5 * 60 * 1000);
+        this.subscribedTicketsTimer = QACommon.interval(this.getSubscribedTickets, (5 * 60 * 1000) + 200);
     },
     methods: {
         openTicketDetails(ticketId) {
@@ -22,6 +24,11 @@ const TicketsApp = {
         getOwnedTickets() {
             $.getJSON(API_TICKETS_URL, (data) => {
                 this.ownedTickets = data;
+            });
+        },
+        getSubscribedTickets() {
+            $.getJSON(API_SUBSCRIBED_TICKETS_URL, (data) => {
+                this.subscribedTickets = data;
             });
         },
         getIconClass(status) {
