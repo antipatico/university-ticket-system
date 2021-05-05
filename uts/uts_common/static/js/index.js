287e07a1e017c32ef2f38ac284e47e62fe3b7e57
antipatico <antipatico@github.com>
2021-05-05 17:20:23 +0200

    Ticketd opened time, ugly ugly code

--- uts/uts_common/static/js/index.js
+++ uts/uts_common/static/js/index.js
@@ -1,5 +1,8 @@
 const TicketsApp = {
     delimiters: ['$%', '%$'],
+    components: {
+      'li-ticket': TicketListItem,
+    },
     data() {
         return {
             recentTickets: null,
@@ -16,9 +19,6 @@ const TicketsApp = {
         this.subscribedTicketsTimer = QACommon.interval(this.getSubscribedTickets, (5 * 60 * 1000) + 200);
     },
     methods: {
-        openTicketDetails(ticketId) {
-            window.location.href = TICKET_DETAILS_URL + ticketId
-        },
         getRecentTickets() {
             $.getJSON(API_RECENT_ACTIVITIES_URL, (data) => {
                 this.recentTickets = data;
@@ -34,6 +34,12 @@ const TicketsApp = {
                 this.subscribedTickets = data;
             });
         },
+    }
+}
+const app = Vue.createApp(TicketsApp);
+
+app.mixin({
+    methods: {
         getIconClass(status) {
             return QACommon.getIconClass(status);
         },
@@ -42,7 +48,55 @@ const TicketsApp = {
         },
         dateToString(date) {
             return QACommon.dateToString(date);
-        }
+        },
+        dateDiffFromNow(date) {
+            date = new Date(date);
+            let now = new Date();
+            let secondsDiff = (now - date) / 1000;
+            if (secondsDiff < 10) {
+                return "ora";
+            } else if (secondsDiff < 60) {
+                return `${secondsDiff} secondi fa`;
+            } else {
+                let minutesDiff = secondsDiff / 60;
+                if (minutesDiff < 2) {
+                    return "un minuto fa";
+                } else if (minutesDiff < 60) {
+                    return `${minutesDiff} minuti fa`;
+                } else {
+                    let hoursDiff = minutesDiff / 60;
+                    if (hoursDiff < 2) {
+                        return "un'ora fa";
+                    } else if (hoursDiff < 24) {
+                        return `${hoursDiff} ore fa`;
+                    } else {
+                        let daysDiff = hoursDiff / 24;
+                        if (daysDiff < 2) {
+                            return "un giorno fa";
+                        } else if (daysDiff < 30) {
+                            return `${daysDiff} giorni fa`;
+                        } else {
+                            let monthsDiff = daysDiff / 30;
+                            if (monthsDiff < 2) {
+                                return "un mese fa";
+                            } else if (monthsDiff < 12) {
+                                return `${monthsDiff} mesi fa`
+                            } else {
+                                let yearsDiff = monthsDiff / 12;
+                                if (monthsDiff < 2) {
+                                    return "un anno fa";
+                                }
+                                return `${yearsDiff} anni fa`;
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        openTicketDetails(ticketId) {
+            window.location.href = TICKET_DETAILS_URL + ticketId
+        },
     }
-}
-Vue.createApp(TicketsApp).mount('#ticketsApp')
+});
+
+app.mount("#ticketsApp");
