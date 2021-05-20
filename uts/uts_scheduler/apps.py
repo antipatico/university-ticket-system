222145ff8e5d3ebb5cb105ecf48c7163fbbd76b9
antipatico <antipatico@github.com>
2021-05-20 18:59:10 +0200

    Removed broken code

--- uts/uts_scheduler/apps.py
+++ uts/uts_scheduler/apps.py
@@ -3,12 +3,3 @@ from django.apps import AppConfig
 
 class UtsSchedulerConfig(AppConfig):
     name = 'uts_scheduler'
-    def ready(self):
-        from django_q.models import Schedule
-        if not Schedule.objects.filter(name="delete_unused_ticket_event_attachments").exists():
-            Schedule.objects.create(
-                name="delete_unused_ticket_event_attachments",
-                func='uts_scheduler.schedules.delete_unused_ticket_event_attachments',
-                schedule_type=Schedule.CRON,
-                cron="0 3 * * * *",  # Schedule every day at 3 A.M.
-            )
